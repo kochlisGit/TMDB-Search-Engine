14e27fcdb4094d890ef72d6d51378348bed6c2aa
Vasileios Kochliaridis <kohliaridis@Hotmail.com>
2022-08-19 17:31:44 +0300

    Delete indexes directory

--- indexes/operations/IndexOperation.java
+++
@@ -1,7 +0,0 @@
-package indexes.operations;
-
-import indexes.indexes.Index;
-
-public interface IndexOperation {
-    boolean executeOperation(Index index);
-}
--- indexes/readers/EntityExtractor.java
+++
@@ -1,9 +0,0 @@
-package indexes.readers;
-
-import org.apache.lucene.document.Document;
-
-import java.util.List;
-
-public interface EntityExtractor<T> {
-    List<T> extractEntities(List<Document> docList);
-}
